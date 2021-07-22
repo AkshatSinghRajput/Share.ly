02997af604d311f40fdc1a634fe6f21058af854b
Akshat Singh Rajput <Akshatsinghrajput201@gmail.com>
2021-07-22 20:19:20 +0530

    final-commit

--- Config/db.js
+++ Config/db.js
@@ -1,7 +1,8 @@
-//require('dotenv').config();
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }).then(() => {
+mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
+.then(() => {
     console.log('DataBase Connected!');
 }).catch((error => {
     console.log('Connection failed: ' + error);
