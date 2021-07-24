9fc4d05da6ae756060fcad23a028cfb7caf2feb3
Akshat Singh Rajput <Akshatsinghrajput201@gmail.com>
2021-07-24 12:38:11 +0530

    final-commit

--- routes/register.js
+++ routes/register.js
@@ -33,54 +33,54 @@ router.post('/', (req, res) => {
                     req.flash('error', 'User with this email already exists!') // delete this after rendering the flash message.
                     res.redirect('/register');
                 } else {
-                    mailsend(email, 'Your otp for registration is ' + otp);
-                    req.flash('success_msg', 'Otp is sended on your email')
-                    res.render('realOtp');
-                    router.post('/otp', (req, res) => {
-                        if (req.body.otp == otp) {
-                            // here we have to write a code to verify the email provided by the user.
-                            bcrypt.hash(password, 10, (err, hash) => {
-                                if (err) {
-                                    // here we have to render a error page.
-                                    console.log(err);
-                                    res.redirect('/error');
-                                } else {
-                                    const u1 = new User({
-                                        name: name,
-                                        email: email,
-                                        uniqueid: md5(name + email),
-                                        password: hash
-                                    });
-                                    u1.save();
-                                    // here we have to flash a message that user register successfully.
-                                    req.flash('success_msg', 'Registered successfully!') // delete this after rendering the flash message
-                                    res.redirect('/login');
-                                }
-                            });
-                        } else {
-                            req.flash('error', 'Otp is wrong try again!');
-                            res.redirect('/register');
-                        }
-                    })
-                    // // here we have to write a code to verify the email provided by the user.
-                    // bcrypt.hash(password, 10, (err, hash) => {
-                    //     if (err) {
-                    //         // here we have to render a error page.
-                    //         console.log(err);
-                    //         res.redirect('/error');
-                    //     } else {
-                    //         const u1 = new User({
-                    //             name: name,
-                    //             email: email,
-                    //             uniqueid: md5(name + email),
-                    //             password: hash
+                    // mailsend(email, 'Your otp for registration is ' + otp);
+                    // req.flash('success_msg', 'Otp is sended on your email')
+                    // res.render('realOtp');
+                    // router.post('/otp', (req, res) => {
+                    //     if (req.body.otp == otp) {
+                    //         // here we have to write a code to verify the email provided by the user.
+                    //         bcrypt.hash(password, 10, (err, hash) => {
+                    //             if (err) {
+                    //                 // here we have to render a error page.
+                    //                 console.log(err);
+                    //                 res.redirect('/error');
+                    //             } else {
+                    //                 const u1 = new User({
+                    //                     name: name,
+                    //                     email: email,
+                    //                     uniqueid: md5(name + email),
+                    //                     password: hash
+                    //                 });
+                    //                 u1.save();
+                    //                 // here we have to flash a message that user register successfully.
+                    //                 req.flash('success_msg', 'Registered successfully!') // delete this after rendering the flash message
+                    //                 res.redirect('/login');
+                    //             }
                     //         });
-                    //         u1.save();
-                    //         // here we have to flash a message that user register successfully.
-                    //         req.flash('success_msg', 'Registered successfully!') // delete this after rendering the flash message
-                    //         res.redirect('/login');
+                    //     } else {
+                    //         req.flash('error', 'Otp is wrong try again!');
+                    //         res.redirect('/register');
                     //     }
-                    // });
+                    // })
+                    // // here we have to write a code to verify the email provided by the user.
+                    bcrypt.hash(password, 10, (err, hash) => {
+                        if (err) {
+                            // here we have to render a error page.
+                            console.log(err);
+                            res.redirect('/error');
+                        } else {
+                            const u1 = new User({
+                                name: name,
+                                email: email,
+                                uniqueid: md5(name + email),
+                                password: hash
+                            });
+                            u1.save();
+                            // here we have to flash a message that user register successfully.
+                            req.flash('success_msg', 'Registered successfully!') // delete this after rendering the flash message
+                            res.redirect('/login');
+                        }
+                    });
                 }
             }
         });
